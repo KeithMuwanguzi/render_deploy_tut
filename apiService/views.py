05524a303e905a4ce95d740e02a87b7e0edceea1
keith <keithmuwanguzi@gmail.com>
2024-09-26 23:31:30 +0300

    Correct student

--- apiService/views.py
+++ apiService/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-from .models import Student
+from students.models import Student
 from students.serializers import StudentSerializer
 
 @api_view(['GET'])
