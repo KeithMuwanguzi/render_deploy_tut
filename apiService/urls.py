79630e855559fec19e2c9cf54ca735062a2939b9
keith <keithmuwanguzi@gmail.com>
2024-09-26 23:29:58 +0300

    Students added

--- apiService/urls.py
+++ apiService/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.testEnd, name='test'),
+    path('students/', views.get_students, name='get_students'),       
+    path('students/<int:student_id>/', views.get_student, name='get_student'), 
+    path('students/add/', views.add_student, name='add_student'), 
 ]
--- apiService/views.py
+++ apiService/views.py
@@ -1,5 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
+from .models import Student
+from students.serializers import StudentSerializer
 
 @api_view(['GET'])
 def testEnd(request):
@@ -8,3 +11,47 @@ def testEnd(request):
         "message": "This is working properly"
     }
     return Response(data)
+
+@api_view(['POST'])
+def add_student(request):
+    if request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'status':'success',
+                'data': serializer.data,
+            })
+        else:
+            return Response({
+                'status':'error',
+                'error':serializer.errors,
+            })
+
+@api_view(['GET'])
+def get_students(request):
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response({
+            'status': 'success',
+            'data': serializer.data
+        })
+
+@api_view(['GET'])
+def get_student(request, student_id):
+    try:
+        student = Student.objects.get(id=student_id)
+    except Student.DoesNotExist:
+        return Response({
+            'status': 'error',
+            'data': 'Student not found'
+        }, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = StudentSerializer(student)
+        return Response({
+            'status': 'success',
+            'data': serializer.data
+        })
+
